0d06a02f19a785b59d06e5e10244e93c1abe1128
知莫 <117190706+Agan-ippe@users.noreply.github.com>
2026-06-05 19:03:30 +0800

    chore:更新一下包名

---
+++ src/main/java/com/project_name/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.project_name.exception;
+
+import com.project_name.common.BaseResponse;
+import com.project_name.common.ErrorCode;
+import com.project_name.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ * @author <a href="https://github.com/Agan-ippe">知莫</a>
+ */
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("BusinessException", e);
+        return ResultUtils.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("RuntimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, "系统错误");
+    }
+}
